3615c20e5fe8477afb956fcdaaef4b839fda1c4a
Rui Baltazar <rui.p.baltazar@gmail.com>
2016-09-02 01:05:41 +0800

    [Finishes] Flexible about personal profile and users

--- app/models/personal_profile.rb
+++ app/models/personal_profile.rb
@@ -1,7 +1,7 @@
 class PersonalProfile < ActiveRecord::Base
   belongs_to :user
 
-  delegate :update_search_string, to: :user
+  delegate :update_search_string, to: :user, allow_nil: true
 
   before_save :update_search_string, if: -> { first_name_changed? || middle_name_changed? || last_name_changed? }
 end
