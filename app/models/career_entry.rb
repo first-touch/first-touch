f5ba01eb14c73c80f1d99322cf5ac62c896afcee
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-22 12:26:29 +0800

    [Hotfix] fixed career entry

--- app/models/career_entry.rb
+++ app/models/career_entry.rb
@@ -3,14 +3,13 @@ class CareerEntry < ApplicationRecord
   belongs_to :club
 
   validates_presence_of :start_date
-  validate :start_date_before_end_date, if: -> { start_date.present? && end_date.present? }
+  validate :start_date_before_end_date,
+           if: -> { start_date.present? && end_date.present? }
 
   private
 
   def start_date_before_end_date
-    unless start_date < end_date
-      errors[:start_date] = "has to be earlier than end date"
-    end
+    return if start_date < end_date
+    errors.add(:start_date, I18n.t('carrer.invalid_dates'))
   end
-
 end
--- spec/models/career_entry_spec.rb
+++ spec/models/career_entry_spec.rb
@@ -9,15 +9,23 @@ RSpec.describe CareerEntry, type: :model do
     it { is_expected.to validate_presence_of :start_date }
 
     it 'checks if start date is before end date' do
-      invalid_entry = build :career_entry, start_date: Date.today, end_date: Date.yesterday
+      invalid_entry = build :career_entry,
+                            start_date: Date.today,
+                            end_date: Date.yesterday
       expect(invalid_entry).to be_invalid
+      errors = invalid_entry.errors.full_messages
+      expect(errors).to eq ['Start date has to be earlier than end date']
 
-      valid_entry = build :career_entry, start_date: Date.yesterday, end_date: Date.today
+      valid_entry = build :career_entry,
+                          start_date: Date.yesterday,
+                          end_date: Date.today
       expect(valid_entry).to be_valid
     end
 
     it 'accepts nil for end date' do
-      valid_entry = build :career_entry, start_date: Date.yesterday, end_date: nil
+      valid_entry = build :career_entry,
+                          start_date: Date.yesterday,
+                          end_date: nil
       expect(valid_entry).to be_valid
     end
   end
