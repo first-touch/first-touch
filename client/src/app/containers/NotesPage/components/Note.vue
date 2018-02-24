4dc1d9d131fece45c7f9131d076dd6e0b3687da5
yaojie <limyaojie93@gmail.com>
2018-02-24 18:29:07 +0800

    + img to note component

--- client/src/app/containers/NotesPage/components/Note.vue
+++ client/src/app/containers/NotesPage/components/Note.vue
@@ -3,6 +3,7 @@
   <img src={{ info.image }}
   <div class="info">
     <h2 class="title">{{ info.name }}</h2>
+    <img :src=info.image_url />
     <span v-for="tag in info.tags"  :key="tag" v-on:click="noteFn(tag)">
       {{ tag }}
     </span>
