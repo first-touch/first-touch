3209058def93d475ce4e2dbec893e279df336142
yaojie <limyaojie93@gmail.com>
2018-02-25 09:29:00 +0800

    changed note_spec, add comment on tag query for note#index

--- app/concepts/v1/note/representer/full.rb
+++ app/concepts/v1/note/representer/full.rb
@@ -9,7 +9,7 @@ module V1
         property :name
         property :content
         property :image_url
-        collection :all_tags_list, as: :tags
+        collection :all_tags_list, as: :tags # need to clean up collection to prevent multiple queries
       end
     end
   end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,14 +3,9 @@ require 'rails_helper'
 RSpec.describe Note, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to :user }
-    it { is_expected.to have_one :image }
   end
 
   describe 'validations' do
-    before do
-      allow(subject).to receive(:process_image)
-    end
-
     it { is_expected.to validate_presence_of :name }
   end
 end
