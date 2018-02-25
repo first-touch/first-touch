ccf4d0b5bb0097f1783af94ae7d04e7e01f5fad2
yaojie <limyaojie93@gmail.com>
2018-02-25 10:10:05 +0800

    set stub Note tests

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Note, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of :name }
   end
+
+
+  describe 'Note ML Fields' do
+    it 'should have a valid enumeration' do
+      pending 'finalize field_types'
+      fail
+    end
+    it 'should have a valid JSON structure' do
+      pending 'finalize field_elements'
+      fail
+    end
+  end
+
 end
