8a19b6b01aa868208bbf850293c55cc17cf111bc
oualid handoura <oualid.handoura@alphanova.org>
2018-04-13 11:00:18 +0800

    Last change of code format

--- db/migrate/20180411094531_refactor_reports_table_after_review.rb
+++ db/migrate/20180411094531_refactor_reports_table_after_review.rb
@@ -1,4 +1,4 @@
-class RefactorAfterReview < ActiveRecord::Migration[5.1]
+class RefactorReportsTableAfterReview < ActiveRecord::Migration[5.1]
   def change
     add_column :reports, :meta_data, :json
     drop_table :report_data
