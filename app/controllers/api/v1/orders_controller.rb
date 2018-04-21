66047e0b4a94acaea8a4b5cc2197d17b06fe199a
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-04-21 11:30:22 +0800

    WIP - cleanup rubocop

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,11 +3,11 @@ require './lib/payment_util'
 module Api
   module V1
     class OrdersController < Api::V1::BaseController
-      # Todo:will change once stripe is set
+      # TODO: will change once stripe is set
       def create
-          result = ::V1::Report::Buy.(params,current_user: current_user)
-          response = FirstTouch::Endpoint.(result)
-          render json: response[:data], status: response[:status]
+        result = ::V1::Report::Buy.(params, current_user: current_user)
+        response = FirstTouch::Endpoint.(result)
+        render json: response[:data], status: response[:status]
       end
     end
   end
