21bed6a8df631c562576aac81d37e72f2eb53cdf
oualid handoura <oualid.handoura@alphanova.org>
2018-05-03 18:00:52 +0800

    Stripe Constants for backend And Frontend Adding Loading Components will be used later

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+
+Rails.configuration.stripe = {
+  publishable_key: Rails.application.secrets.stripe[:publishable_key],
+  secret_key:  	Rails.application.secrets.stripe[:secret_key]
+}
+
+::Stripe.api_key = Rails.application.secrets.stripe[:secret_key]
