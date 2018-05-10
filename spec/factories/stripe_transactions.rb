7d104125a8eaf589ebac0df7193846f198fa02e6
oualid handoura <oualid.handoura@alphanova.org>
2018-05-10 16:57:23 +0800

    Stripe:    Remove of automatic payout    Save the charge transaction id to database (for futur refund... ) in new model Stripe_transaction

---
+++ spec/factories/stripe_transactions.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :stripe_transaction do
+    stripe_id "MyText"
+    refounded false
+    refound_at "2018-05-10"
+    order nil
+    type_transaction "MyText"
+  end
+end
