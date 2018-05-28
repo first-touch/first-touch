7fad2f9f8730f006b3b6b2e01ecec6ff14e38e75
oualid handoura <oualid.handoura@alphanova.org>
2018-05-28 10:01:05 +0800

    fix code

--- app/concepts/v1/competition/operation/index.rb
+++ app/concepts/v1/competition/operation/index.rb
@@ -11,9 +11,6 @@ module V1
           competition_list = competition_list.where(
             'name ILIKE ?', "%#{query_params}%"
           )
-          if FirstTouch::REGISTERABLE_ROLE_NAMES.include? params[:role]
-            competition_list = competition_list.with_role params[:role]
-          end
           options['models'] = competition_list
         end
       end
