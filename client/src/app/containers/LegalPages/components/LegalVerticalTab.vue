014b428690b83bdc14d895bdc66fe9c2dd8669a3
stephanikoykoy <lim065@mymail.sim.edu.sg>
2018-08-17 10:18:00 +0800

    WIP reduced padding

--- client/src/app/containers/LegalPages/components/LegalVerticalTab.vue
+++ client/src/app/containers/LegalPages/components/LegalVerticalTab.vue
@@ -5,7 +5,7 @@
     </div>
     <b-nav vertical>
       <b-collapse is-nav visible id="menu-contents" class="mt-2 d-lg-block">
-        <div class="contents-vertical-tab d-block pr-lg-4" id="navbar-contents">
+        <div class="contents-vertical-tab d-block" id="navbar-contents">
           <b-card class="vertical-tab">
             <div class="section-separator"></div>
             <b-nav-item>
@@ -43,46 +43,41 @@
     top: 0;
     height: 100vh;
     position: sticky;
-    padding-top: 0.5em;
-    padding-right: 1em;
-    padding-left: 1em;
+    .card-body {
+      padding: 0.7em;
+    }
+    .vertical-tab {
+      background: #343434;
+      padding-top: 9em;
+      border: 0;
+    }
+    .vertSideItem {
+      margin-bottom: 0.5em;
+      margin-top: 0.5em;
+      color: #7F8081;
+      text-transform: uppercase;
+      font-weight: 100;
+    }
+    .contents-vertical-tab .router-link-active {
+      border-left: 2px solid #A8CB5C;
+      color: #A8CB5C;
+    }
+    .vertSideItem:hover {
+      color: #A8CB5C;
+    }
+    .contents-vertical-tab .section-separator {
+      border-bottom: 1px solid #7F8081;
+    }
   }
 
-  .vertical-tab {
-    background: #343434;
-    padding-top: 8em;
-    border: 0;
-  }
-
-  .vertSideItem {
-    margin-bottom: 0.5em;
-    margin-top: 0.5em;
-    color: #7F8081;
-    text-transform: uppercase;
-    font-weight: 100;
-  }
-
-  .contents-vertical-tab .router-link-active {
-    /*code does not work*/
-    border-left: 2px solid #A8CB5C;
-    color: #A8CB5C;
-  }
-
-  .vertSideItem:hover {
-    color: #A8CB5C;
-  }
-
-  .contents-vertical-tab .section-separator {
-    border-bottom: 1px solid #7F8081;
-  }
 
   @media(max-width: 992px) {
     #legal-verticaltab {
       height: auto;
       padding-bottom: 0.5em;
-    }
-    .vertical-tab {
-      padding-top: 0em;
+      .vertical-tab {
+        padding-top: 0em;
+      }
     }
   }
 </style>
