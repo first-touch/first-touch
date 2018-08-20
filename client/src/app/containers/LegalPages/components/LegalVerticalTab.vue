58bfa325dcc5b071274a0b6d17dc73a0686a9aa0
Stephanie Nicole <32569739+stephanikoykoy@users.noreply.github.com>
2018-08-20 19:55:50 +0800

    [Resolves #159586482] Legal pages - fix orientation and create vertical tab for policies
    # Story Title
    
    [Legal Pages - fix orientation and create vertical tab for policies](https://www.pivotaltracker.com/story/show/159586482)
    
    # Changes made
    
    - created three columns 
    - added left vertical tab
    - moved table of contents to the right
    - made left vertical tab and table of contents to be "sticky" along the page
    
    # How does the solution addresses the problem
    
    - Includes other file legal contents into the different individual files so that user cannot state ignorance. 
    - Includes the table of contents of each individual file for easy manoeuvre within the page

---
+++ client/src/app/containers/LegalPages/components/LegalVerticalTab.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="legal-verticaltab">
+    <div class="d-block d-lg-none menu-btn">
+      <b-btn v-b-toggle.menu-contents>MENU</b-btn>
+    </div>
+    <b-nav vertical>
+      <b-collapse is-nav visible id="menu-contents" class="mt-2 d-lg-block">
+        <div class="contents-vertical-tab d-block" id="navbar-contents">
+          <b-card class="vertical-tab">
+            <div class="section-separator"></div>
+            <b-nav-item>
+              <router-link class="nav-link vertSideItem" to="/terms_conditions">User Agreement</router-link>
+            </b-nav-item>
+            <div class="section-separator"></div>
+            <b-nav-item>
+              <router-link class="nav-link vertSideItem" to="/privacy_policy">Privacy Policy</router-link>
+            </b-nav-item>
+            <div class="section-separator"></div>
+            <b-nav-item>
+              <router-link class="nav-link vertSideItem" to="#">Help (Report a Problem)</router-link>
+            </b-nav-item>
+            <div class="section-separator"></div>
+          </b-card>
+        </div>
+      </b-collapse>
+    </b-nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .menu-btn>.btn-secondary,
+  .menu-btn>.btn-secondary:active,
+  .menu-btn>.btn-secondary:focus,
+  .menu-btn>.btn-secondary:not([disabled]):not(.disabled).active,
+  .btn-secondary:not([disabled]):not(.disabled):active,
+  .show>.btn-secondary.dropdown-toggle {
+    background: #343434;
+    color: #7F8081;
+    margin: 1em;
+  }
+
+  #legal-verticaltab {
+    background: #343434;
+    top: 0;
+    position: sticky;
+    position: -webkit-sticky;
+    .card-body {
+      padding: 0.7em;
+    }
+    .vertical-tab {
+      background: #343434;
+      padding-top: 9em;
+      border: 0;
+      height: 100vh;
+    }
+    .vertSideItem {
+      margin-bottom: 0.5em;
+      margin-top: 0.5em;
+      color: #7F8081;
+      text-transform: uppercase;
+      font-weight: 100;
+    }
+    .contents-vertical-tab .router-link-active {
+      border-left: 2px solid #A8CB5C;
+      color: #A8CB5C;
+    }
+    .vertSideItem:hover {
+      color: #A8CB5C;
+    }
+    .contents-vertical-tab .section-separator {
+      border-bottom: 1px solid #7F8081;
+    }
+  }
+
+
+  @media(max-width: 992px) {
+    #legal-verticaltab {
+      .vertical-tab {
+        padding-top: 0em;
+        padding-bottom: 0.5em;
+        height: auto;
+      }
+    }
+  }
+</style>
+
+<script>
+  export default {
+    name: 'LegalVerticalTab',
+    props: {
+      sticky: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+</script>
