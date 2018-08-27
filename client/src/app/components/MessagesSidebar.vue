8443c57f5779166beeae206a9fd25ac1fbd2eb15
stephanikoykoy <lim065@mymail.sim.edu.sg>
2018-08-27 16:27:19 +0800

    WIP collapsing button on mobile devices

--- client/src/app/components/MessagesSidebar.vue
+++ client/src/app/components/MessagesSidebar.vue
@@ -2,35 +2,46 @@
   <div class="sidenav-right">
     <div class="container">
       <div class="info-blk">
-
         <div class="info-blk-header row">
-          <span class="info-blk-header-name">
-            All Messages
+          <span class="col-8 no-padding">
+            <span class="info-blk-header-name">
+              All Messages
+            </span>
+          </span>
+          <span class="col-3 no-padding">
+            <b-btn class="d-block d-lg-none" v-b-toggle.menu-contents>
+              <div class="bar">
+              </div>
+              <div class="bar">
+              </div>
+              <div class="bar">
+              </div>
+            </b-btn>
           </span>
         </div>
 
-
-        <div class="row">
-          <div class="info-blk-body">
-
-            <div class="sidebar-widget">
-              <div class="row d-md-flex flex-md-row">
-                <div class="col no-padding">
-              <input v-model="searchTerm" type="text" class="search" placeholder="Type a name" />
-                </div>
-                <div class="col no-padding">
-              <select class="sort" v-model="sortTerm">
-                <option disabled>Sort</option>
-                <option>Name</option>
-                <option>Club</option>
-              </select>
+        <b-collapse is-nav visible id="menu-contents" class="d-lg-block">
+          <b-card>
+            <div class="row">
+              <div class="info-blk-body">
+                <div class="sidebar-widget">
+                  <div class="row d-md-flex flex-md-row">
+                    <div class="col no-padding">
+                      <input v-model="searchTerm" type="text" class="search" placeholder="Type a name" />
+                    </div>
+                    <div class="col no-padding">
+                      <select class="sort" v-model="sortTerm">
+                        <option disabled>Sort</option>
+                        <option>Name</option>
+                        <option>Club</option>
+                      </select>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-
-
-          </div>
-        </div>
+          </b-card>
+        </b-collapse>
 
         <div class="inbox">
           <inbox-entry v-for="chat in chats" :chat="chat" :active="active(chat.chat_with.id)" :id="chat.chat_with.id" :key="chat.id"
@@ -48,8 +59,29 @@
     height: calc(100vh - 119px);
     box-shadow: -2px 2px 2px #555;
   }
+  .bar {
+    box-sizing: border-box;
+    border-top: 1px solid white;
+    width: 15px;
+    margin: 3px;
+  }
+  .collapse {
+    margin-left: -15px;
+  }
+  .btn {
+    padding: 5px;
+    background-color: #7F8081;
+  }
+  .card {
+    background: transparent;
+    .card-body {
+      padding: 0;
+      .info-blk-body {
+        margin-left: 15px;
+      }
+    }
+  }
   .sidebar-widget {
-    display: flex;
     .search,
     .sort {
       margin-bottom: 5%;
@@ -57,8 +89,8 @@
       background-color: transparent;
       color: #fff;
       padding: 2px 7px;
-      margin-left: 16px;
       height: 25px;
+      margin-left: 16px;
     }
     .search {
       border-radius: 4px;
