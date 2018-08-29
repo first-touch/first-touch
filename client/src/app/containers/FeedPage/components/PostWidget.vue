7526db25e637c69260b495693686bf4f7c3a11c3
stephanikoykoy <lim065@mymail.sim.edu.sg>
2018-08-29 16:39:13 +0800

    WIP changed code to bootstrap vue

--- client/src/app/containers/FeedPage/components/PostWidget.vue
+++ client/src/app/containers/FeedPage/components/PostWidget.vue
@@ -2,34 +2,67 @@
   <div>
     <div class="timeline-widget">
       <div class="arrow"></div>
-      <button class="timeline-widget-button btn-post" @click="toggle">
-        publish a new post
-      </button>
-      <button class="timeline-widget-button btn-upload">
-        upload a picture
-      </button>
+      <b-btn class="timeline-widget-button" v-b-toggle.post-text>
+        <img src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png">
+        Publish a new post
+      </b-btn>
+      <b-btn class="timeline-widget-button" v-b-toggle.post-media>
+        <img src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png">
+        Upload a picture
+      </b-btn>
     </div>
-    <div class="timeline-widget writer active">
-      <div class="container-fluid">
-        <form @submit.prevent="handleSubmit">
-          <fieldset class="form-group">
-            <div :value="content" @keyup="handleContentChange" class="form-control" rows="4">
-              <textarea placeholder="Share a thought, link or opinion"></textarea>
-              <div class="row justify-content-end">
-                <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
-                <button v-else type="submit" class="bar-button">Post</button>
-              </div>
+    <b-collapse id="post-text" accordion="post-upload">
+      <b-card>
+        <div class="container">
+          <form @submit.prevent="handleSubmit">
+            <fieldset class="form-group">
+              <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="Share a thought, link or opinion"></textarea>
+            </fieldset>
+            <div class="row justify-content-end">
+              <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
+              <button v-else type="submit" class="bar-button">Post</button>
             </div>
-          </fieldset>
+          </form>
+        </div>
+      </b-card>
+    </b-collapse>
+    <b-collapse id="post-media" accordion="post-upload">
+      <b-card>
+        <div class="container">
+          <form @submit.prevent="handleSubmit">
+            <fieldset class="form-group">
+              <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="uploadMedia"></textarea>
+            </fieldset>
+            <div class="row justify-content-end">
+              <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
+              <button v-else type="submit" class="bar-button">Post</button>
+            </div>
+          </form>
+        </div>
+      </b-card>
+    </b-collapse>
 
-        </form>
-      </div>
-    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   @import '~stylesheets/variables.scss';
+
+  #post-text,
+  #post-media {
+    border-left: 7px solid $secondary-header-color;
+    padding-left: 20px;
+    textarea {
+      border: none;
+      color: #7F8081;
+    }
+    .bar-button {
+      border: none;
+      box-shadow: none;
+      background: #B3CB75;
+      width: 200px;
+    }
+  }
 
   .timeline-widget {
     display: flex;
@@ -41,56 +74,15 @@
       border: none;
       max-width: 12vw;
       margin-right: 20px;
-      &.btn-post {
-        padding-left: 50px;
-        background-image: url('https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png');
-        background-repeat: no-repeat;
-        background-position: left;
-        background-size: contain;
-      }
-      &.btn-upload {
-        padding-left: 50px;
-        background-image: url('https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png');
-        background-repeat: no-repeat;
-        background-position: left;
-        background-size: contain;
+      margin-bottom: 20px;
+
+      img {
+        height: 50px;
       }
     }
     .arrow {
       margin-top: 18px;
       border-left-color: $secondary-header-color;
-    }
-    &.writer {
-      height: 0;
-      overflow: hidden;
-      transition: height 1s;
-      &.active {
-        height: auto;
-      }
-      .container-fluid {
-        padding: 10px 20px 0;
-        &.active {
-          height: auto;
-        }
-        form {
-          display: flex;
-          flex-direction: column;
-          margin-bottom: -15px;
-          textarea {
-            width: 100%;
-            color:  #7F8081;
-            padding: 10px;
-            border: none;
-          }
-          .bar-button {
-            background-color: #B3CB75;
-            border: none;
-            box-shadow: none;
-            margin-right: 10px;
-            width: 200px;
-          }
-        }
-      }
     }
   }
 </style>
