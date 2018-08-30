30d98ad2e6bc06760276685f04bc810567c9ff9c
stephanikoykoy <lim065@mymail.sim.edu.sg>
2018-08-30 11:16:39 +0800

    WIP - changed to bootstrap vue

--- client/src/app/containers/FeedPage/components/PostWidget.vue
+++ client/src/app/containers/FeedPage/components/PostWidget.vue
@@ -2,145 +2,165 @@
   <div>
     <div class="timeline-widget">
       <div class="arrow"></div>
-      <button class="timeline-widget-button" @click="toggleWrite">
-        <button class="btn-png"></button>
-        <span class="btn-text">
-          publish a new post
-        </span>
-      </button>
-      <button class="timeline-widget-button" @click="toggleUpload">
-        <button class="btn-png"></button>
-        <span class="btn-text">
-          upload a picture
-        </span>
-      </button>
+      <div class="timeline-widget-button">
+        <b-btn v-b-toggle.post-text>
+          <div class="row">
+            <div class="col no-padding">
+              <img src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png">
+            </div>
+            <div class="col">
+              Publish a
+              <br> new post
+            </div>
+          </div>
+        </b-btn>
+      </div>
+      <div class="timeline-widget-button">
+        <b-btn v-b-toggle.post-media>
+          <div class="row">
+            <div class="col no-padding">
+              <img src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png">
+            </div>
+            <div class="col">
+              Upload
+              <br> a photo
+            </div>
+          </div>
+        </b-btn>
+      </div>
     </div>
-
-    <div id="file-drag-drop" class="timeline-widget media" :class="{ active }">
-      <div class="item-container">
-        <form ref="fileform" @submit.prevent="handleSubmit">
-          <fieldset class="form-group form-control" rows="4">
-            <span class="drop-files">
-              <b-form-file id="input" :value="content" @change="browsedFiles()" placeholder="Browse for files..."
-                multiple></b-form-file>
-              <br> OR Drop files here...
-            </span>
-            <div class="files-uploaded">
-              <div v-for="(file, key) in files" class="file-listing">
-                <img class="preview" v-bind:ref="'preview'+parseInt( key )" /> {{ file.name }}
-                <div class="remove-container">
-                  <a class="remove" v-on:click="removeFile( key )">Remove</a>
+    <b-collapse id="post-text" accordion="post-upload">
+      <b-card>
+        <div class="container">
+          <form @submit.prevent="handleSubmit">
+            <fieldset class="form-group">
+              <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="Share a thought, link or opinion"></textarea>
+            </fieldset>
+            <div class="row justify-content-end">
+              <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
+              <button v-else type="submit" class="bar-button">Post</button>
+            </div>
+          </form>
+        </div>
+      </b-card>
+    </b-collapse>
+    <b-collapse id="post-media" accordion="post-upload">
+      <b-card>
+        <div class="container">
+          <form ref="fileform" @submit.prevent="handleSubmit">
+            <fieldset class="form-group form-control">
+              <span class="drop-files">
+                <b-form-file id="input" :value="content" @change="browsedFiles" placeholder="Drop files here or Browse Files here..."
+                  multiple></b-form-file>
+                <br> OR Drop files here...
+              </span>
+              <div class="files-uploaded">
+                <div v-for="(file, key) in files" class="file-listing">
+                  <div class="remove-container">
+                    <a class="remove" v-on:click="removeFile( key )"><img src="/images/cancel.png"></a>
+                  </div>
+                  <div class="images-uploaded">
+                    <img class="preview" v-bind:ref="'preview'+parseInt( key )" /> {{ file.name }}
+                  </div>
                 </div>
               </div>
+            </fieldset>
+
+            <div class="row justify-content-end">
+              <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
+              <button v-else type="submit" class="bar-button">Post</button>
             </div>
-          </fieldset>
-          <div class="post-button d-flex justify-content-end">
-            <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
-            <button v-else-if="files.length > 0" v-on:click="submitFiles()" type="submit" class="bar-button">Post</button>
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <div class="timeline-widget writer" :class="{ active }" v-if="writePost">
-      <div class="item-container">
-        <form @submit.prevent="handleSubmit">
-          <fieldset class="form-group">
-            <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="What's on your mind?"></textarea>
-          </fieldset>
-          <div class="post-button d-flex justify-content-end">
-            <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
-            <button v-else type="submit" class="bar-button">Post</button>
-          </div>
-        </form>
-      </div>
-    </div>
-
+          </form>
+        </div>
+      </b-card>
+    </b-collapse>
 
   </div>
 </template>
+
 <style lang="scss" scoped>
   @import '~stylesheets/variables.scss';
+
   .timeline-widget {
     display: flex;
     border-left: 7px solid $secondary-header-color;
-    .item-container {
-      padding: 10px 20px 0;
-      width: 100%;
-      &.active {
-        height: auto;
-      }
-    }
+
     .timeline-widget-button {
-      background-color: $navbar-background-color;
-      border: none;
-      margin-right: 20px;
-      .btn-png {
-        box-sizing: content-box;
-        width: 33px;
-        height: 40px;
-        background-color: $navbar-background-color;
-        border: none;
-        background-image: url('https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png');
-        background-repeat: no-repeat;
-        background-position: left;
-        background-size: contain;
-      }
-      .btn-text {
-        box-sizing: content-box;
-        max-width: 6vw;
+      .btn-secondary {
+        border: none; // max-width: 12vw;
+        margin-right: 20px;
+        margin-bottom: 20px;
         color: $secondary-text-color;
         text-transform: uppercase;
+        background-color: $navbar-background-color;
         text-align: left;
-        float: right;
+      }
+
+      .btn-text-title {
+        margin-top: 10px;
+      }
+
+      img {
+        height: 50px;
       }
     }
+
     .arrow {
       margin-top: 18px;
       border-left-color: $secondary-header-color;
     }
-    &.writer {
-      overflow: hidden;
-      transition: height 1s;
+  }
+
+  #post-text,
+  #post-media {
+    border-left: 7px solid $secondary-header-color;
+    padding-left: 20px;
+
+    textarea {
+      border: none;
+      color: #7F8081;
     }
-    &.media {
-      fieldset {
-        background: #fff;
-        min-height: 200px;
-      }
-      .drop-files {
-        padding: 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        color: #7F8081;
-      }
-      .files-uploaded {
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        overflow: auto;
-      }
-      .file-listing {
-        padding: 10px;
-        display: flex;
-        flex-direction: column;
-        width: 200px;
-      }
-      .file-listing img {
-        height: 100px;
-        width: 150px;
-      }
-      .remove-container a {
-        color: red;
-        cursor: pointer;
-      }
+
+    .bar-button {
+      border: none;
+      box-shadow: none;
+      background: #B3CB75;
+      width: 200px;
     }
   }
 
-  .w-100 {
-    width: 25% !important;
+  .form-control {
+    border: none;
+  }
+
+  #post-media {
+    .files-uploaded {
+      .file-listing {
+        margin-top: 25px;
+
+        .images-uploaded {
+          padding-top: 7px;
+          padding-left: 10px;
+
+          img {
+            height: 100px;
+            margin: 5px;
+          }
+        }
+
+        .remove-container {
+          .remove {
+            position: absolute;
+            cursor: pointer;
+
+            img {
+              height: 20px;
+            }
+          }
+        }
+      }
+    }
+
   }
 </style>
 
@@ -150,32 +170,22 @@
     props: ['posting', 'handleContentChange', 'handleSubmit', 'content'],
     data() {
       return {
-        active: false,
-        writePost: false,
-        mediaPost: false,
         dragAndDropCapable: false,
         files: [],
         uploadPercentage: 0,
       };
     },
     methods: {
-      toggleWrite() {
-        this.$set(this, 'active', true);
-        this.$set(this, 'writePost', !this.writePost);
-        this.$set(this, 'mediaPost', false);
-      },
-      toggleUpload() {
-        this.$set(this, 'active', true);
-        this.$set(this, 'mediaPost', !this.mediaPost);
-        this.$set(this, 'writePost', false);
-      },
       browsedFiles() {
         var inputElement = document.getElementById("input");
         inputElement.addEventListener("change", handleFiles, false);
+
         function handleFiles() {
           var fileList = this.files;
           for (var i = 0; i < fileList.length; i++) {
+            console.log(fileList[i]);
             return fileList[i];
+            debugger;
           }
         }
       },
@@ -201,29 +211,29 @@
           }
         }
       },
-      submitFiles() {
-        let formData = new FormData();
-        for (var i = 0; i < this.files.length; i++) {
-          let file = this.files[i];
+      // submitFiles() {
+      //   let formData = new FormData();
+      //   for (var i = 0; i < this.files.length; i++) {
+      //     let file = this.files[i];
 
-          formData.append('files[' + i + ']', file);
-        }
-        axios.post('/file-drag-drop',
-            formData, {
-              headers: {
-                'Content-Type': 'multipart/form-data'
-              },
-              onUploadProgress: function (progressEvent) {
-                this.uploadPercentage = parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total));
-              }.bind(this)
-            }
-          ).then(function () {
-            console.log('SUCCESS!!');
-          })
-          .catch(function () {
-            console.log('FAILURE!!');
-          });
-      },
+      //     formData.append('files[' + i + ']', file);
+      //   }
+      //   axios.post('/post-media',
+      //       formData, {
+      //         headers: {
+      //           'Content-Type': 'multipart/form-data'
+      //         },
+      //         onUploadProgress: function (progressEvent) {
+      //           this.uploadPercentage = parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total));
+      //         }.bind(this)
+      //       }
+      //     ).then(function () {
+      //       console.log('SUCCESS!!');
+      //     })
+      //     .catch(function () {
+      //       console.log('FAILURE!!');
+      //     });
+      // },
       removeFile(key) {
         this.files.splice(key, 1);
       },
