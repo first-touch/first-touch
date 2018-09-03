c66c4a2d94628d77083e930a8956bd1b84a98e9e
Stephanie Nicole <32569739+stephanikoykoy@users.noreply.github.com>
2018-09-03 22:45:58 +0800

    [Resolves #157213810 , #157213809 , #160011770 , #160014459 , #157213806] Amend design for Posting (#65)
    # Story Title
    
    [HOME - Amend design for Posting](https://www.pivotaltracker.com/story/show/157213810)
    
    [HOME - Animation for Posting is fine but make it faster](https://www.pivotaltracker.com/story/show/157213809)
    
    [ Publish Post - edit height](https://www.pivotaltracker.com/story/show/160011770)
    
    [HOME - timeline upload texts overlap on icon on mobile devices and smaller resized window](https://www.pivotaltracker.com/story/show/160014459)
    
    [HOME - Text formatting next to buttons is inconsistent - the words should be left aligned](https://www.pivotaltracker.com/story/show/157213806)
    
    # Changes made
    
    - Put Post button in the white area at bottom right and 
    - the button should not have a border, box should be #B3CB75 and the text #FFFFFF 
    - the line on the left should only align to the bottom of the white box 
    - the text within the box should read ïShare a thought, link or opinionÍ 
    - this text should be in #7F8081 and smaller and slightly removed from the edges
    - changed to bootstrap vue code = animation became faster, height is default auto
    - made sure text and icon don't overlap when window is reduced and on small devices
    - made sure text next to buttons is consistent and words are left aligned
    
    # How does the solution addresses the problem
    
    - Amends design for Posting
    - timeline upload texts do not overlap on icon on mobile devices and text are left aligned
    
    ![screen shot 2018-08-24 at 4 42 34 pm](https://user-images.githubusercontent.com/32569739/44574704-bd837780-a7bc-11e8-9761-4604693741fb.png)
    ![screen shot 2018-08-24 at 4 42 06 pm](https://user-images.githubusercontent.com/32569739/44574705-be1c0e00-a7bc-11e8-93c6-29b86179661e.png)

--- client/src/app/containers/FeedPage/components/PostWidget.vue
+++ client/src/app/containers/FeedPage/components/PostWidget.vue
@@ -2,96 +2,128 @@
   <div>
     <div class="timeline-widget">
       <div class="arrow"></div>
-      <button class="timeline-widget-button btn-post" @click="toggle">
-        publish a new post
-      </button>
-      <button class="timeline-widget-button btn-upload">
-        upload a picture
-      </button>
-    </div>
-    <div class="timeline-widget writer" :class="{ active }">
-      <div class="container">
-        <form @submit.prevent="handleSubmit">
-          <fieldset class="form-group">
-            <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="What's on your mind?"></textarea>
-          </fieldset>
-          <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
-          <button v-else type="submit" class="bar-button">Post</button>
-        </form>
+      <div class="timeline-widget-button">
+        <b-btn v-b-toggle.post-text>
+          <div class="row">
+            <div class="col no-padding">
+              <img src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png">
+            </div>
+            <div class="col">
+              Publish a
+              <br>
+              new post
+            </div>
+          </div>
+        </b-btn>
+      </div>
+      <div class="timeline-widget-button">
+        <b-btn v-b-toggle.post-media>
+          <div class="row">
+            <div class="col no-padding">
+              <img src="https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png">
+            </div>
+            <div class="col">
+              Upload
+              <br>
+              a photo
+            </div>
+          </div>
+        </b-btn>
       </div>
     </div>
+    <b-collapse id="post-text" accordion="post-upload">
+      <b-card>
+        <div class="container">
+          <form @submit.prevent="handleSubmit">
+            <fieldset class="form-group">
+              <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="Share a thought, link or opinion"></textarea>
+            </fieldset>
+            <div class="row justify-content-end">
+              <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
+              <button v-else type="submit" class="bar-button">Post</button>
+            </div>
+          </form>
+        </div>
+      </b-card>
+    </b-collapse>
+    <b-collapse id="post-media" accordion="post-upload">
+      <b-card>
+        <div class="container">
+          <form @submit.prevent="handleSubmit">
+            <fieldset class="form-group">
+              <textarea :value="content" @keyup="handleContentChange" class="form-control" rows="4" placeholder="uploadMedia"></textarea>
+            </fieldset>
+            <div class="row justify-content-end">
+              <button v-if="posting" type="button" class="bar-button" disabled>Posting...</button>
+              <button v-else type="submit" class="bar-button">Post</button>
+            </div>
+          </form>
+        </div>
+      </b-card>
+    </b-collapse>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
-@import '~stylesheets/variables.scss';
+  @import '~stylesheets/variables.scss';
 
-.timeline-widget {
-  display: flex;
-  border-left: 7px solid $secondary-header-color;
-  .timeline-widget-button {
-    color: $secondary-text-color;
-    text-transform: uppercase;
-    background-color: $navbar-background-color;
-    border: none;
-    max-width: 12vw;
-    margin-right: 20px;
-    &.btn-post {
-      padding-left: 50px;
-      background-image: url('https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png');
-      background-repeat: no-repeat;
-      background-position: left;
-      background-size: contain;
+  #post-text,
+  #post-media {
+    border-left: 7px solid $secondary-header-color;
+    padding-left: 25px;
+    textarea {
+      border: none;
+      color: #7F8081;
     }
-    &.btn-upload {
-      padding-left: 50px;
-      background-image: url('https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/photo512x512.png');
-      background-repeat: no-repeat;
-      background-position: left;
-      background-size: contain;
+    .bar-button {
+      border: none;
+      box-shadow: none;
+      background: #B3CB75;
+      width: 200px;
     }
   }
-  .arrow {
-    margin-top: 18px;
-    border-left-color: $secondary-header-color;
-  }
-  &.writer {
-    height: 0;
-    overflow: hidden;
-    transition: height 1s;
-    &.active {
-      height: 180px;
-    }
-    .container {
-      padding: 10px 20px 0;
-      &.active {
-        height: auto;
+
+  .timeline-widget {
+    display: flex;
+    border-left: 7px solid $secondary-header-color;
+    .timeline-widget-button {
+      .btn-secondary {
+        border: none; // max-width: 12vw;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        color: $secondary-text-color;
+        text-transform: uppercase;
+        background-color: $navbar-background-color;
+        text-align: left;
       }
-      form {
-        display: flex;
-        flex-direction: column;
-        .bar-button {
-          align-self: flex-end;
-        }
+      .btn-text-title {
+        margin-top: 10px;
+      }
+      img {
+        height: 50px;
       }
     }
+    .arrow {
+      margin-top: 18px;
+      border-left-color: $secondary-header-color;
+    }
   }
-}
 </style>
 
 <script>
-export default {
-  name: 'PostWidget',
-  props: ['posting', 'handleContentChange', 'handleSubmit', 'content'],
-  data() {
-    return {
-      active: false,
-    };
-  },
-  methods: {
-    toggle() {
-      this.$set(this, 'active', !this.active);
+  export default {
+    name: 'PostWidget',
+    props: ['posting', 'handleContentChange', 'handleSubmit', 'content'],
+    data() {
+      return {
+        active: false,
+      };
     },
-  },
-};
+    methods: {
+      toggle() {
+        this.$set(this, 'active', !this.active);
+      },
+    },
+  };
 </script>
