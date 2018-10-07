b963c104ca4b24ee4afbb0599fd5e1b17a3a85f3
shihaotan91 <shihaotan91@gmail.com>
2018-10-07 21:35:55 +0800

    WIP - update error message when club is registere

--- client/src/app/containers/SignupPage/components/RegisterClub.vue
+++ client/src/app/containers/SignupPage/components/RegisterClub.vue
@@ -140,7 +140,7 @@ export default {
         .then(res => res.status === 200 && res.json())
         .then(({has_owner}) => {
           if (has_owner) {
-            return this.$set(this, 'error', "You can't register youself as a director for a club with an existing owner!");
+            return this.$set(this, 'error', "The club that you have chosen already has an existing director! You can choose to sign in to your account first while we email the club's director");
           } else {
             return this.$set(this, 'error', null);
           }
