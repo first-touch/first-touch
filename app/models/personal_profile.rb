b9a534b56a2c4d93b5c8aacbbcd90b9de80ec91c
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-10-23 19:41:09 +0800

    [Hotfix] cleanup personal profile model

--- app/models/personal_profile.rb
+++ app/models/personal_profile.rb
@@ -3,5 +3,11 @@ class PersonalProfile < ApplicationRecord
 
   delegate :update_search_string, to: :user, allow_nil: true
 
-  before_save :update_search_string, if: -> { first_name_changed? || middle_name_changed? || last_name_changed? }
+  before_save :update_search_string, if: -> { name_changed? }
+
+  private
+
+  def name_changed?
+    first_name_changed? || middle_name_changed? || last_name_changed?
+  end
 end
