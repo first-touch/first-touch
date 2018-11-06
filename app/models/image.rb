77f084f17db41cbfddab22b2b9ddf5d277369183
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-11-06 12:32:52 +0800

    WIP - commented paperclip logic in image

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,24 +2,24 @@ class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true, touch: true
   validates_presence_of :imageable
 
-  has_attached_file :file,
-                    styles: {
-                      thumb: '100x100\>',
-                      medium: '300x300\>',
-                      resized: '600x600\>'
-                    },
-                    default_style: :resized,
-                    s3_protocol: 'https'
+  # has_attached_file :file,
+  #                   styles: {
+  #                     thumb: '100x100\>',
+  #                     medium: '300x300\>',
+  #                     resized: '600x600\>'
+  #                   },
+  #                   default_style: :resized,
+  #                   s3_protocol: 'https'
 
-  validates_attachment_content_type :file, content_type: [
-    'image/jpg',
-    'image/jpeg',
-    'image/bmp',
-    'image/png',
-    'image/gif',
-    'image/x-ms-bmp'
-  ]
-  validates_attachment_presence :file
+  # validates_attachment_content_type :file, content_type: [
+  #   'image/jpg',
+  #   'image/jpeg',
+  #   'image/bmp',
+  #   'image/png',
+  #   'image/gif',
+  #   'image/x-ms-bmp'
+  # ]
+  # validates_attachment_presence :file
 
   def url
     file.url
