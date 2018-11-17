d0f75cc424a3996f1c72eaba410c35baf260941c
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-11-17 10:46:34 +0800

    WIP - cleanup spec folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'rspec/json_expectations'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'paperclip/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -58,8 +57,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include Paperclip::Shoulda::Matchers
 end
 
 Shoulda::Matchers.configure do |config|
