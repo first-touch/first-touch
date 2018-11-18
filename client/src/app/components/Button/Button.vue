48155808c42bab0c156b838fd1cdda9bed9880d2
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-11-18 11:59:56 +0800

    WIP - init extracting the button to a component

---
+++ client/src/app/components/Button/Button.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="timeline-widget-button">
+  <button @click="onClick">
+    <slot>
+      <span class="fa fa-camera"></span>
+      Button
+    </slot>
+  </button>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
--- client/src/app/containers/MessagesPage/index.vue
+++ client/src/app/containers/MessagesPage/index.vue
@@ -3,6 +3,10 @@
     <div class="container-fluid">
       <div class="ft-page messages">
         <h4 class="spaced-title upper-cased main-color">Messages</h4>
+        <ft-button :on-click="consoleClick">
+          <span class="fa fa-notes"></span>
+          Write Message
+        </ft-button>
         <router-view v-if="currentChatWith"></router-view>
       </div>
     </div>
@@ -23,11 +27,13 @@ body {
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import MessagesSidebar from 'app/components/MessagesSidebar';
+import Button from 'app/components/Button/Button';
 
 export default {
   name: 'Messages',
   components: {
     'messages-sidebar': MessagesSidebar,
+    'ft-button': Button,
   },
   computed: {
     ...mapGetters(['messages', 'user', 'token']),
@@ -37,6 +43,9 @@ export default {
   },
   methods: {
     ...mapActions(['reloadConversation', 'sendMessage']),
+    consoleClick() {
+      console.log('Button clicked')
+    }
   },
 };
 </script>
