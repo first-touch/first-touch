0a869e5898a675c5fe95fe8c8280542395e888d1
shihaotan91 <shihaotan91@gmail.com>
2018-12-26 13:51:46 +0800

    WIP - add html structure to modal

--- client/src/app/components/NewMessage/NewMessageFormPopup.vue
+++ client/src/app/components/NewMessage/NewMessageFormPopup.vue
@@ -1,17 +1,56 @@
 <template>
   <div>
-    <p>Hello World</p>
+    <div class="row">
+      <div class="col-lg-5">
+        <h4 class="name">Write a new message</h4>
+      </div>
+      <div class="col-lg-7">
+        <div class="form-inline float-right">
+          <div class="form-group mx-1">
+            <button class="form-control a-bar-button">Send</button>
+          </div>
+          <div class="form-group mx-1">
+            <button class="form-control a-bar-button reverse">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="form">
+      <fieldset class="form-group col-md-12">
+        <input type="users" v-model="user_ids" class="form-control" placeholder="Type a name" />
+      </fieldset>
+      <fieldset class="form-group col-md-12">
+        <input type="subject" v-model="subject" class="form-control" placeholder="Give this conversation a name(optional)" />
+      </fieldset>
+      <fieldset class="form-group col-md-12">
+        <input type="body" v-model="body" class="form-control" placeholder="Write your message..." />
+      </fieldset>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-  p {
-    color: black;
+  @import '~stylesheets/variables';
+
+  .row {
+    margin-bottom: 30px;
   }
+
+  h4 {
+    color: $main-text-color;
+  }
+
 </style>
 
 <script>
 export default {
   name: 'NewMessagePopup',
+  data() {
+    return {
+      user_ids: [],
+      subject: '',
+      body: '',
+    }
+  }
 };
 </script>
