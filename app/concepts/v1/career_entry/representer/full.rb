bfaedb54dfed287fd68992e371dd9933807bbb0a
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-01-28 19:38:29 +0800

    [Hotfix] Fixed career entry representer and club representer path

---
+++ app/concepts/v1/career_entry/representer/full.rb
@@ -0,0 +1,14 @@
+module V1
+  module CareerEntry
+    module Representer
+      class Full < Representable::Decorator
+        include Representable::JSON
+
+        property :id
+        property :start_date
+        property :end_date
+        property :club, extend: ::V1::Club::Representer::Full
+      end
+    end
+  end
+end
