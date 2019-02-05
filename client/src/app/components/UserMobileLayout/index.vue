1f59126bc3195b6ff52ad1e8291368b3fbcaa30d
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-02-05 13:03:10 +0800

    WIP - commented search bar for now

--- client/src/app/components/UserMobileLayout/index.vue
+++ client/src/app/components/UserMobileLayout/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrapper">
     <mobile-navbar></mobile-navbar>
+    <!--  <search-bar :search="getSearchResults" :results="searchResult.value" :placeholder="placeholder" :action="action" /> -->
     <router-view></router-view>
   </div>
 </template>
@@ -14,12 +15,23 @@
 </style>
 
 <script>
+  import { mapGetters, mapActions } from 'vuex';
   import MobileNavbar from 'app/components/UserLayout/components/MobileNavbar';
+  import SearchBar from '../SearchBar';
 
   export default {
     name: 'UserMobileLayout',
     components: {
-      'mobile-navbar': MobileNavbar
+      'mobile-navbar': MobileNavbar,
+      'search-bar': SearchBar
+    },
+    data() {
+      return {
+        placeholder: 'Search for players, clubs, events'
+      }
+    },
+    computed: {
+      ...mapGetters(['searchResult']),
     }
   };
 </script>
