6b0151adf242e1616ce1899be2f92f228503bfb6
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-03-26 13:44:55 +0800

    [Hotfix] Fixed registerable roles

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 module FirstTouch
-  REGISTERABLE_ROLE_NAMES = %w[player scout manager coach director].freeze
+  REGISTERABLE_ROLE_NAMES = %w[player scout agent director].freeze
   REDIS_NOTES_SUBSCRIBE_CHANNEL = 'yustynn-channel'.freeze
   REDIS_NOTES_PUBLISH_CHANNEL = 'yustynn-channel-channel'.freeze
 
