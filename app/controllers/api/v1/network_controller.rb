31bf4b055c687bf5992f2dfb620d35a94fa632ef
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-03-27 13:46:57 +0800

    [Hotfix] Removed unnecessary code

--- app/controllers/api/v1/network_controller.rb
+++ app/controllers/api/v1/network_controller.rb
@@ -5,17 +5,6 @@ module Api
         op = ::V1::Network::Index.(params: params, current_user: @current_user)
         response = FirstTouch::Endpoint.(op, ::V1::Network::Representer::Index)
         render json: response[:data], status: response[:status]
-
-        # render json: { message: 'Deprecated' }, status: :ok
-        # TODO: Needs improvement as network grows
-        # will probably be impossible to build the whole graph at once
-        # network_attrs = {
-        #   followers: @current_user.followers,
-        #   following: @current_user.following
-        # }
-        # network = Network.new(network_attrs)
-
-        # render json: NetworkSerializer.new(network), root: 'network'
       end
     end
   end
