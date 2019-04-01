23b8da0252f2707e3a94be6d481a20d707cb73ab
Rui <rui.p.baltazar@gmail.com>
2019-04-01 23:54:16 +0800

    [Resolves 1_6] Fix navbar
    # Story Title
    
    Fix Navbar
    
    # Changes made
    
    - made authenticated and non authenticated Navbars follow the same style
    - Fixed dropdown for scout module in auth Navbar
    
    # How does the solution addresses the problem
    
    Makes the UI consistence across the app. dropdown work properly

--- client/src/app/components/LandingNavbar.vue
+++ client/src/app/components/LandingNavbar.vue
@@ -1,23 +1,23 @@
 <template>
-  <nav id="landing-navbar" class="navbar navbar-expand-lg sticky-top">
-    <a class="navbar-brand" href="/">
-      <img src="/images/landing-page/ft-navbar-logo.png" alt="Ft logo">
-    </a>
+  <nav id="landing-navbar" class="navbar navbar-expand-lg bg-dark sticky-top">
+    <router-link to="/" class="navbar-brand">
+      <img class="img-fluid" src="/images/landing-page/ft-navbar-logo.png" alt="Ft Logo" />
+    </router-link>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-nav" aria-controls="navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbar-nav">
-      <ul class="navbar-nav ml-auto list-unstyled">
-        <li>
-          <router-link to="/about">About</router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav navbar-right ml-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
         </li>
-        <li>
-          <router-link to="/users/sign_up">Sign Up</router-link>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/users/sign_up">Sign Up</router-link>
         </li>
-        <li>
-          <router-link to="/users/sign_in">Login</router-link>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/users/sign_in">Login</router-link>
         </li>
       </ul>
     </div>
@@ -26,41 +26,8 @@
 
 <style lang="scss" scoped>
   @import '~stylesheets/variables';
-
-  // TODO: Duplicated with Mobile Navbar. Find a way to share the style
-  #landing-navbar {
-    ul {
-      p {
-        padding: 10px;
-      }
-
-      li a {
-        padding: 10px;
-        font-size: 1.1em;
-        display: block;
-        text-transform: uppercase;
-      }
-
-      li a:hover {
-        background: $nav-hover-background;
-        color: $secondary-text-color;
-        text-decoration: none;
-      }
-
-      li a.router-link-exact-active.router-link-active, a[aria-expanded="true"] {
-        color: $main-header-color;
-      }
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    #landing-navbar {
-     .navbar-brand {
-      img {
-        height: 40px;
-        }
-      }
-    }
+  li a.router-link-exact-active.router-link-active, a[aria-expanded="true"] {
+    color: $main-header-color;
   }
 </style>
 
