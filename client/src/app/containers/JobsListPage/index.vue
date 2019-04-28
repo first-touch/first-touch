d04ed2a764f304d734dc0444f2a38c8c181204f7
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-04-28 19:03:44 +0800

    WIP - initial cleanup

--- client/src/app/containers/JobsListPage/index.vue
+++ client/src/app/containers/JobsListPage/index.vue
@@ -1,24 +1,16 @@
 <template>
   <div>
-    <div class="container-fluid">
-      <div class="ft-page">
+    <div class="ft-page">
+      <div class="container">
         <actions class="widget"/>
-          <bidslist class="widget" />
-          <reportlist class="widget" />
+        <bids-list class="widget" />
+        <report-list class="widget" />
       </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-@import '~stylesheets/variables';
-.widget {
-  margin-bottom: 20px;
-}
-</style>
-
 <script>
-import NotificationSidebar from 'app/components/NotificationSidebar';
 import ReportList from './components/ReportsList';
 import BidsList from './components/BidsList';
 import Actions from './components/Actions';
@@ -26,10 +18,9 @@ import Actions from './components/Actions';
 export default {
   name: 'JobList',
   components: {
-    sidebar: NotificationSidebar,
-    actions: Actions,
-    reportlist: ReportList,
-    bidslist: BidsList
+    Actions,
+    ReportList,
+    BidsList
   },
   data() {
     return {
