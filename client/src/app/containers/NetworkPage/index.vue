31480653a99fe463b03b391916696b394db4b367
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-05-01 13:03:15 +0800

    WIP - removed extra div

--- client/src/app/containers/NetworkPage/index.vue
+++ client/src/app/containers/NetworkPage/index.vue
@@ -1,30 +1,28 @@
 <template>
-  <div>
-    <div class="container ft-page">
-      <h4 class="spaced-title upper-cased main-color page-title mb-5">My network</h4>
-      <div class="row network-filters justify-content-center mb-4">
-        <div class="col-xl-6 ml-auto mr-auto">
-          <select class="form-control network-widget-sort" v-model="role">
-            <option value="">All Roles</option>
-            <option value="agent">Agent</option>
-            <option value="director">Director</option>
-            <option value="player">Player</option>
-            <option value="scout">Scout</option>
-          </select>
-        </div>
-        <div class="col-xl-6 ml-auto mr-auto">
-          <input type="text" class="form-control network-widget-search" placeholder="Type a name" v-model="searchTerm" @keyup.prevent="filterNetwork()" />
-        </div>
+  <div class="container ft-page">
+    <h4 class="spaced-title upper-cased main-color page-title mb-5">My network</h4>
+    <div class="row network-filters justify-content-center mb-4">
+      <div class="col-xl-6 ml-auto mr-auto">
+        <select class="form-control network-widget-sort" v-model="role">
+          <option value="">All Roles</option>
+          <option value="agent">Agent</option>
+          <option value="director">Director</option>
+          <option value="player">Player</option>
+          <option value="scout">Scout</option>
+        </select>
       </div>
-      <div class="row network-container justify-content-center">
-        <div class="col-xl-12" v-if="loading">
-          <h4 class="text-center">Loading...</h4>
-        </div>
-        <div class="col-xl-12">
-          <network-item v-for="item in items"
-              :info="item"
-              :key="item.id" />
-        </div>
+      <div class="col-xl-6 ml-auto mr-auto">
+        <input type="text" class="form-control network-widget-search" placeholder="Type a name" v-model="searchTerm" @keyup.prevent="filterNetwork()" />
+      </div>
+    </div>
+    <div class="row network-container justify-content-center">
+      <div class="col-xl-12" v-if="loading">
+        <h4 class="text-center">Loading...</h4>
+      </div>
+      <div class="col-xl-12">
+        <network-item v-for="item in items"
+            :info="item"
+            :key="item.id" />
       </div>
     </div>
   </div>
