b3e774d3533a994bc9f49df388aa3729430f71a8
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-05-01 16:16:37 +0800

    WIP - cleaning up errors when searching

--- app/controllers/api/v1/competitions_controller.rb
+++ app/controllers/api/v1/competitions_controller.rb
@@ -3,7 +3,8 @@ module Api
     class CompetitionsController < Api::V1::BaseController
       def index
         result = ::V1::Competition::Index.(params: params, current_user: current_user)
-        render json: result[:models], status: :ok
+        response = FirstTouch::Endpoint.(result, ::V1::Competition::Representer::Index)
+        render json: response[:data], status: response[:status]
       end
     end
   end
--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -3,7 +3,8 @@ module Api
     class TeamsController < Api::V1::BaseController
       def index
         result = ::V1::Team::Index.(params: params, current_user: current_user)
-        render json: result[:models], status: :ok
+        response = FirstTouch::Endpoint.(result, ::V1::Team::Representer::Index)
+        render json: response[:data], status: response[:status]
       end
 
       def show
