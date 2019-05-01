7492ee5124fbd86228f80cc101d94b60f5b9827f
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-05-01 15:57:31 +0800

    WIP - using ft buttons and removed actions component

--- client/src/app/components/Button/Button.vue
+++ client/src/app/components/Button/Button.vue
@@ -1,18 +1,33 @@
 <template>
-<button @click="onClick" class="a-button header-button">
-  <div class="icon-wrapper">
-    <v-icon :name="icon" scale="1" class="round-icon"/>
-  </div>
-  <slot>
-    Button
-  </slot>
-</button>
+  <button v-if="onClick" @click="onClick" class="a-button header-button">
+    <div class="icon-wrapper">
+      <v-icon :name="icon" scale="1.5" class="round-icon"/>
+    </div>
+    <slot>
+      Button
+    </slot>
+  </button>
+
+  <router-link v-else class="a-button header-button" :to="linkTo">
+    <div class="icon-wrapper">
+      <v-icon :name="icon" scale="1.5" class="round-icon"/>
+    </div>
+    <slot>
+      Button
+    </slot>
+  </router-link>
 </template>
 
 <style lang="scss" scoped>
   @import '~stylesheets/variables';
 
-  button {
+  a {
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  button, a {
     &:hover {
       .icon-wrapper {
         background-color: lighten($secondary-header-color, 10%);
@@ -23,13 +38,14 @@
   }
 
   .icon-wrapper {
-    width: 30px;
-    height: 30px;
+    width: 50px;
+    height: 50px;
     text-align: center;
     background-color: $secondary-header-color;
     color: $first-touch-white;
     border-radius: 50%;
     display: inline-block;
+    line-height: 45px;
   }
 
   .header-button {
@@ -46,17 +62,23 @@
 
   export default {
     name: 'FtButton',
-    components:{
+    components: {
       VIcon
     },
     props: {
       onClick: {
         type: Function,
-        required: true
+        default: undefined
       },
       icon: {
         type: String,
         default: 'clipboard'
+      },
+      linkTo: {
+        type: Object,
+        default: function() {
+          return {};
+        }
       }
     }
   }
