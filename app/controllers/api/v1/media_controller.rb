12ce53899f4e5691e2018c45bcea580bb27ce811
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-06-13 00:11:13 +0800

    WIP - added destroy operation for medium

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -8,11 +8,9 @@ module Api
       end
 
       def destroy
-        u = User.find_by(id: params['user_id'])
-        media_id = params['id']
-        media = u.media.find media_id
-        media.purge
-        render json: {}, status: :ok
+        res = ::V1::Medium::Destroy.(params: params, current_user: current_user)
+        response = FirstTouch::Endpoint.(res)
+        render json: response[:data], status: response[:status]
       end
     end
   end
