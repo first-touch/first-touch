6cf9fb76e3d8912b5f5a75d4a51cc122b47b7b4b
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-06-26 14:13:55 +0800

    WIP - clean up preferred foot selector

--- client/src/app/components/Input/PreferredFoot.vue
+++ client/src/app/components/Input/PreferredFoot.vue
@@ -1,41 +1,24 @@
 <template>
-  <vselect :disabled="readonly" ref="vSelect" v-model="model" :onChange="update" :options="options" :placeholder="placeholder" :clearable="true" />
+  <v-select :disabled="readonly" ref="vSelect" v-model="value" :onChange="update" :options="options" :placeholder="placeholder" :clearable="true" />
 </template>
 
 <script>
-  import vSelect from 'vue-select';
+  import VSelect from 'vue-select';
 
   export default {
     name: 'PreferredFoot',
-    props: ['value','readonly','placeholder'],
+    props: ['value', 'readonly', 'placeholder'],
     components: {
-      vselect: vSelect
-    },
-    mounted: function () {
-        const index = this.$options.filters.searchInObj(this.options, option => option.value === this.value)
-        this.model = this.options[index];
+      VSelect
     },
     data() {
       return {
-        model: null,
-        options: [{
-            label: 'Left',
-            value: 'L'
-          },
-          {
-            label: 'Right',
-            value: 'R'
-          },
-          {
-            label: 'Both',
-            value: 'B'
-          }
-        ]
+        options: ['Left', 'Right', 'Both']
       };
     },
     methods: {
       update(val) {
-        if (val) this.$emit('update:val', this.$options.filters.vueSelect2Val(val));
+        this.$emit('update:val', val);
       }
     }
   };
