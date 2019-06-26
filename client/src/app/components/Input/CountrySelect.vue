15c2b585f8beab2bc57110404f33ec7fc80a5964
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-06-26 16:55:54 +0800

    WIP - fixed country select

--- client/src/app/components/Input/CountrySelect.vue
+++ client/src/app/components/Input/CountrySelect.vue
@@ -1,59 +1,40 @@
 <template>
-  <vselect :disabled="readonly" ref="vSelect" v-model="model" :onChange="update" :options="countries" :placeholder="placeholder" :clearable="true" />
+  <v-select
+    :disabled="readonly"
+    ref="vSelect"
+    v-model="value"
+    :onChange="update"
+    :options="countries"
+    :placeholder="placeholder"
+    :clearable="true"
+    index="value"
+  />
 </template>
 
 <script>
 import countrydata from 'country-data';
-import vSelect from 'vue-select';
-import $ from 'jquery';
+import VSelect from 'vue-select';
 
 export default {
   name: 'CountrySelect',
-  props: ['value','readonly','placeholder'],
+  props: ['value', 'readonly', 'placeholder'],
   components: {
-    vselect: vSelect
-  },
-  data() {
-    return {
-      model: this.value
-    };
-  },
-  mounted: function() {
-    this.$refs.vSelect.scrollTo = function(){};
-    const index = this.$options.filters.searchInObj(this.countries, option => option.value === this.value);
-    this.model = this.countries[index];
+    VSelect
   },
   methods: {
-    update(val) {
-      this.model = val;
-      if (val && val.value) {
-        this.$emit('update:val', this.$options.filters.vueSelect2Val(val));
-      }
-      else{
-        this.$emit('update:val', null);
-      }
+    update(chosenOption) {
+      this.$emit('update:val', chosenOption);
     }
   },
   computed: {
     countries() {
-      var arr = [];
-      var multiple = [];
-      $.each(countrydata.countries, function(key, value) {
-        if (value.alpha2 && !multiple[value.name]) {
-          multiple[value.name] = 1;
-          arr.push({
-            label: value.name,
-            value: value.alpha2
-          });
-        }
+      let arr = _.map(countrydata.countries.all, function(val) {
+        return {
+          label: val.name,
+          value: val.alpha2
+        };
       });
-      arr.sort(function(a, b) {
-        if (a.label < b.label) return -1;
-        if (a.label > b.label) return 1;
-        return 0;
-      });
-
-      return arr;
+      return _.sortBy(arr, "label");
     }
   }
 };
