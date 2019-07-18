e9b8043968edeb594637f88d2cd54c1878f2c5f6
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-07-18 16:29:06 +0200

    WIP - hack around

--- app/helpers/account_management_helper.rb
+++ app/helpers/account_management_helper.rb
@@ -1,20 +1,20 @@
 module AccountManagementHelper
   def confirm_account_path(confirmation_token)
-    url_base = ActionMailer::Base.default_url_options[:protocol] +
+    url_base = ActionMailer::Base.default_url_options[:protocol] + '://' +
                ActionMailer::Base.default_url_options[:host]
 
     "#{url_base}/users/confirmation?confirmation_token=#{confirmation_token}"
   end
 
   def reset_password_path(reset_token)
-    url_base = ActionMailer::Base.default_url_options[:protocol] +
+    url_base = ActionMailer::Base.default_url_options[:protocol] + '://' +
                ActionMailer::Base.default_url_options[:host]
 
     "#{url_base}/users/new_password?reset_token=#{reset_token}"
   end
 
   def accept_invitation(friend_id, invitation_id)
-    url_base = ActionMailer::Base.default_url_options[:protocol] +
+    url_base = ActionMailer::Base.default_url_options[:protocol] + '://' +
                ActionMailer::Base.default_url_options[:host]
 
     "#{url_base}/users/#{friend_id}/profile?accept_invitation=#{invitation_id}"
