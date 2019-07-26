c660bc56bf927a5ae6596dfc36f60502e0672c8a
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-07-26 14:06:40 +0200

    [Hotfix] fixed access to production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '128.199.193.159', roles: %w[app db web], primary: true, port: 1026
+server '128.199.193.159', user: 'deployer', roles: %w[app db web], primary: true, port: 1026
