4cec2fd7ed394e7902515b18cd9d8446b7201278
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-07-26 10:28:58 +0200

    WIP - sending email with image

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -2,4 +2,9 @@ class CustomDeviseMailer < Devise::Mailer
   helper :account_management # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer'
+
+  def confirmation_instructions(record, token, opts = {})
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/ft-navbar-logo.png")
+    super
+  end
 end
